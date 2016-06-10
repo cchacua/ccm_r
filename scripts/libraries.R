95d777e4690c97780e9bdeba3849e0bd2f8a1e65
Christian Mauricio Chacua Delgado <cchacua@gmail.com>
2016-06-10 00:37:19 -0500

    10062016

---
+++ scripts/libraries.R
@@ -0,0 +1,2 @@
+devtools::install_github("hadley/xml2")
+devtools::install_github("jennybc/googlesheets")
--- scripts/main.R
+++ scripts/main.R
@@ -4,26 +4,7 @@ source("./scripts/functions.R")
 modules<-list.files(path="../data/txt", full.names=TRUE)
 modules
 
-#####################################################################################################
-#House modules
-#####################################################################################################
-
-#Income - expenses module
-Ig_gs_vivienda<-read.delim(files.ig[2], header=TRUE, colClasses = "character")
-#In the whole survey, there are 41.330 houses
-#table(Ig_gs_vivienda$DOMINIO)
-gs_vivienda<-Ig_gs_vivienda[Ig_gs_vivienda$DOMINIO=="CALI A.M.",]
-#There are 1.402 houses in Cali
-rm(Ig_gs_vivienda)
-
-#Labor market module
-Ig_ml_vivienda<-read.delim(files.ig[24], header=TRUE, colClasses = "character")
-
-#A merge of both modules
-#viviendas.cali<-merge(gs_vivienda, Ig_ml_vivienda, by="VIVIENDA", suffixes = c(" ",".del"))
-viviendas.cali<-merge(gs_vivienda, Ig_ml_vivienda[, c("VIVIENDA", setdiff(colnames(Ig_ml_vivienda),colnames(gs_vivienda)))], by="VIVIENDA")
-rm(Ig_ml_vivienda, gs_vivienda)
-write.csv2(viviendas.cali,"../outputs/viviendas.cali.csv")
+#source("./scripts/houses.R")
 
 #####################################################################################################
 #Household modules
