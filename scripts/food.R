2ee7cc87cf81e0b01a7c3473aa8e3bda6761138e
Christian Mauricio Chacua Delgado <cchacua@gmail.com>
2016-06-28 12:21:47 -0500

    28062016 1221

--- scripts/food.R
+++ scripts/food.R
@@ -106,22 +106,60 @@ library(xlsx)
 subclass.dane<-read.xlsx(m.labels[1], sheetName="subclass.dane",encoding="UTF-8")
 
 #File with ENIG_CODE, articles, values and quantities
-gsdu_gas_dia.bigtable<-data.frame(gsdu_gas_dia$CODIGO_ENIG, gsdu_gas_dia$GDU_ARTCLO, gsdu_gas_dia$GDU_VALOR_PGDO_ESTMDO_MES_AJST, gsdu_gas_dia$GDU_CNTDAD_ADQURDA_MES_AJST , gsdu_gas_dia$GDU_UDM_ESTANDAR)
+gsdu_gas_dia.bigtable<-data.frame(gsdu_gas_dia$CODIGO_ENIG, 
+                                  gsdu_gas_dia$GDU_ARTCLO, 
+                                  gsdu_gas_dia$GDU_VALOR_PGDO_ESTMDO_MES_AJST, 
+                                  gsdu_gas_dia$GDU_CNTDAD_ADQURDA_MES_AJST , 
+                                  gsdu_gas_dia$GDU_UDM_ESTANDAR)
 colnames(gsdu_gas_dia.bigtable)<-c("ENIG_CODE", "Product code", "Adjusted monthly value", "Adjusted monthly quantity", "Units")
 
-gsdp_gas_dia.bigtable<-data.frame(gsdp_gas_dia$CODIGO_ENIG, gsdp_gas_dia$GDP_ARTCLO, gsdp_gas_dia$GDP_VALOR_PGDO_ESTMDO_MES_AJST, gsdp_gas_dia$GDP_CNTDAD_ADQURDA_MES_AJST)
+gsdp_gas_dia.bigtable<-data.frame(gsdp_gas_dia$CODIGO_ENIG, 
+                                  gsdp_gas_dia$GDP_ARTCLO, 
+                                  gsdp_gas_dia$GDP_VALOR_PGDO_ESTMDO_MES_AJST, 
+                                  gsdp_gas_dia$GDP_CNTDAD_ADQURDA_MES_AJST)
 colnames(gsdp_gas_dia.bigtable)<-c("ENIG_CODE", "Product code", "Adjusted monthly value", "Adjusted monthly quantity")
 
-gsdu_gasto_alimentos_cap_c.bigtable<-data.frame(gsdu_gasto_alimentos_cap_c$, gsdu_gasto_alimentos_cap_c$ARTICULO, gsdu_gasto_alimentos_cap_c$CANTIDAD,gsdu_gasto_alimentos_cap_c$VALOR_MENSUAL_ALIMENTO,"Product code", "Adjusted monthly quantity", "Adjusted monthly value")
+gsdu_gasto_alimentos_cap_c.bigtable<-data.frame(paste(gsdu_gasto_alimentos_cap_c$VIVIENDA,gsdu_gasto_alimentos_cap_c$HOGAR, sep=""), 
+                                                gsdu_gasto_alimentos_cap_c$ARTICULO,
+                                                gsdu_gasto_alimentos_cap_c$VALOR_MENSUAL_ALIMENTO, 
+                                                gsdu_gasto_alimentos_cap_c$CANTIDAD)
+colnames(gsdu_gasto_alimentos_cap_c.bigtable)<-c("ENIG_CODE", "Product code", "Adjusted monthly value", "Adjusted monthly quantity")
+#View(gsdu_gasto_alimentos_cap_c.bigtable)
 
-gsmf_compra.monthly.bigtable<-data.frame(gsmf_compra$CODIGO_ENIG ,gsmf_compra$GMF_CMPRA_ARTCLO,gsmf_compra$GMF_CMPRA_VLR_PAGO_MES)
+gsmf_compra.monthly.bigtable<-data.frame(gsmf_compra$CODIGO_ENIG ,
+                                         gsmf_compra$GMF_CMPRA_ARTCLO,
+                                         gsmf_compra$GMF_CMPRA_VLR_PAGO_MES)
 colnames(gsmf_compra.monthly.bigtable)<-c("ENIG_CODE", "Product code", "Adjusted monthly value")
-View(gsmf_compra.monthly.bigtable)
+#View(gsmf_compra.monthly.bigtable)
 
 gsmf_forma_adqui.bigtable<-data.frame(gsmf_forma_adqui$CODIGO_ENIG, gsmf_forma_adqui$GMF_ADQU_ARTCLO,gsmf_forma_adqui$GMF_ADQU_VLR_PAGO_MES)
 colnames(gsmf_forma_adqui.bigtable)<-c("ENIG_CODE", "Product code", "Adjusted monthly value")
 
+bigtable.list<-list(gsdu_gas_dia.bigtable[,1:3], 
+                    gsdp_gas_dia.bigtable[,1:3], 
+                    gsdu_gasto_alimentos_cap_c.bigtable[,1:3],
+                    gsmf_compra.monthly.bigtable[,1:3],
+                    gsmf_forma_adqui.bigtable[,1:3])
+
+bigtable.all<-Reduce(function(...) rbind(...), bigtable.list)
+
+bigtable.all$HOUSEID<-substr(bigtable.all$ENIG_CODE, 1, 7)
+bigtable.all$`Adjusted monthly value`<-as.numeric(bigtable.all$`Adjusted monthly value`)
+
+library(datasets)
+library(dplyr)
+bigtable.outone<-summarise(group_by(bigtable.all, HOUSEID, `Product code`), sum(`Adjusted monthly value`, na.rm = TRUE))
+colnames(bigtable.outone)<-c("HOUSEID", "PRODUCTCODE", "VALUE")
+#bigtable.out.houseandproduct<-cast(bigtable.outone, HOUSEID ~ PRODUCTCODE)
+
+
+bigtable.outone$CLASSCODE<-substr(bigtable.outone$PRODUCTCODE, 1, 4)
+
+library(reshape)
+bigtable.out.houseandclass<-cast(bigtable.outone, HOUSEID ~ CLASSCODE, sum, value="VALUE")
 
 
 
 #Average expenses for household that adquires the good
+
+
--- scripts/libraries.R
+++ scripts/libraries.R
@@ -2,8 +2,14 @@ devtools::install_github("hadley/xml2")
 devtools::install_github("jennybc/googlesheets")
 
 
-install.packages("plyr")
-library(plyr)
+#install.packages("plyr")
+#library(plyr)
 
 install.packages("xlsx")
 library(xlsx)
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages('reshape')
+library(reshape)
