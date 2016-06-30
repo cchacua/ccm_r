3256e8dbe414592c460761d74f0c6c062de89b0b
Christian Mauricio Chacua Delgado <cchacua@gmail.com>
2016-06-30 11:11:08 -0500

    30062016 1111

--- scripts/delete.R
+++ scripts/delete.R
@@ -38,3 +38,91 @@ write.xlsx(articles.monthlyu,"../outputs/articles.monthlyu.xlsx")
 
 #sapply(bigtable.out.houseandclass, max)
 #Average expenses for household that adquires the good
+
+
+
+
+
+#############################################################################################
+#Aggregating consumption, quantities and units per article
+#############################################################################################
+
+gsdu_gas_dia.monthly.articles<-aggregate.artquavalunit(gsdu_gas_dia$GDU_ARTCLO, gsdu_gas_dia$GDU_CNTDAD_ADQURDA_MES_AJST , gsdu_gas_dia$GDU_VALOR_PGDO_ESTMDO_MES_AJST, gsdu_gas_dia$GDU_UDM_ESTANDAR,  "Product code", "Adjusted monthly quantity", "Adjusted monthly value", "Units")
+#gsdu_gas_dia.monthly.articles.values<-summarise(group_by(gsdu_gas_dia, GDU_ARTCLO), sum(as.numeric(as.character(GDU_VALOR_PGDO_ESTMDO_MES_AJST)), na.rm = TRUE))
+#View(gsdu_gas_dia.monthly.articles.values)
+gsdp_gas_dia.monthly.articles<-aggregate.artquaval(gsdp_gas_dia$GDP_ARTCLO, gsdp_gas_dia$GDP_CNTDAD_ADQURDA_MES_AJST, gsdp_gas_dia$GDP_VALOR_PGDO_ESTMDO_MES_AJST, "Product code", "Adjusted monthly quantity", "Adjusted monthly value")
+#gsdp_gas_dia.monthly.articles.values<-summarise(group_by(gsdp_gas_dia, GDP_ARTCLO), sum(as.numeric(as.character(GDP_VALOR_PGDO_ESTMDO_MES_AJST)), na.rm = TRUE))
+#View(gsdp_gas_dia.monthly.articles.values)
+gsdu_gasto_alimentos_cap_c.monthly.articles<-aggregate.artquaval(gsdu_gasto_alimentos_cap_c$ARTICULO, gsdu_gasto_alimentos_cap_c$CANTIDAD,gsdu_gasto_alimentos_cap_c$VALOR_MENSUAL_ALIMENTO,"Product code", "Adjusted monthly quantity", "Adjusted monthly value")
+#gsdu_gasto_alimentos_cap_c.monthly.articles.values<-summarise(group_by(gsdu_gasto_alimentos_cap_c, ARTICULO), sum(as.numeric(as.character(VALOR_MENSUAL_ALIMENTO)), na.rm = TRUE))
+#View(gsdu_gasto_alimentos_cap_c.monthly.articles.values)
+gsmf_compra.monthly.articles<-aggregate.twov(gsmf_compra$GMF_CMPRA_ARTCLO,gsmf_compra$GMF_CMPRA_VLR_PAGO_MES, "Product code", "Monthly value")
+#gsmf_compra.monthly.articles.values<-summarise(group_by(gsmf_compra, GMF_CMPRA_ARTCLO), sum(as.numeric(as.character(GMF_CMPRA_VLR_PAGO_MES)), na.rm = TRUE))
+#View(gsmf_compra.monthly.articles.values)
+gsmf_forma_adqui.monthly.articles<-aggregate.twov(gsmf_forma_adqui$GMF_ADQU_ARTCLO,gsmf_forma_adqui$GMF_ADQU_VLR_PAGO_MES, "Product code", "Monthly value")
+#gsmf_forma_adqui.monthly.articles.values<-summarise(group_by(gsmf_forma_adqui, GMF_ADQU_ARTCLO), sum(as.numeric(as.character(GMF_ADQU_VLR_PAGO_MES)), na.rm = TRUE))
+#View(gsmf_forma_adqui.monthly.articles.values)
+
+monthly.articles.list<-list(gsdu_gas_dia.monthly.articles
+                            , gsdp_gas_dia.monthly.articles
+                            , gsdu_gasto_alimentos_cap_c.monthly.articles
+                            , gsmf_compra.monthly.articles
+                            , gsmf_forma_adqui.monthly.articles)
+monthly.articles<-Reduce(function(...) merge(..., all=TRUE , by="Product code"), monthly.articles.list)
+rm(monthly.articles.list, gsdu_gas_dia.monthly.articles
+   , gsdp_gas_dia.monthly.articles
+   , gsdu_gasto_alimentos_cap_c.monthly.articles
+   , gsmf_compra.monthly.articles
+   , gsmf_forma_adqui.monthly.articles)
+
+monthly.articles$class<-substr(monthly.articles$`Product code`, 1, 2)
+monthly.articles<-monthly.articles[monthly.articles$class=="01"  | monthly.articles$class=="02",]
+View(monthly.articles)
+monthly.articles.values<-monthly.articles[,c(1,3,6,8,9,10)]
+View(monthly.articles.values)
+colnames(monthly.articles.values)<-c("Product code", 
+                                     "Value for the household unit database", 
+                                     "Value for the other members receiving income data",
+                                     "Value for the extrapolated food expenses data",
+                                     "Value for the less frequent expenses in cash or credit",
+                                     "Value for the less frequent expenses in gift, barter, etc."
+)
+monthly.articles.values$`Total monthly value`<-rowSums (monthly.articles.values[,2:6], na.rm = TRUE, dims = 1)
+
+#library(xlsx)
+#write.xlsx(monthly.articles.values,"../outputs/monthly.articles.values.xlsx")
+
+
+aggregate.twov= function(v1,v2, title1, title2){
+  mini.df<-data.frame(v1,v2)
+  mini.df<-na.omit(mini.df)
+  out.df<-aggregate(as.numeric(as.character(mini.df[,2])), by=list(Category=mini.df[,1]), FUN=sum)
+  colnames(out.df)<-c(title1, title2)
+  out.df
+}
+
+aggregate.artquaval= function(v1,v2, v3, title1, title2, title3){
+  mini.df<-data.frame(v1,v2,v3)
+  mini.df<-na.omit(mini.df)
+  out.df1<-aggregate(as.numeric(as.character(mini.df[,2])), by=list(Category=mini.df[,1]), FUN=sum)
+  colnames(out.df1)<-c(title1, title2)
+  out.df2<-aggregate(as.numeric(as.character(mini.df[,3])), by=list(Category=mini.df[,1]), FUN=sum)
+  colnames(out.df2)<-c(title1, title3)
+  out.df<-merge(out.df1,out.df2, by=title1)
+  out.df
+}
+
+aggregate.artquavalunit= function(v1,v2, v3, v4, title1, title2, title3, titleunit){
+  mini.df<-data.frame(v1,v2,v3, v4)
+  mini.df<-na.omit(mini.df)
+  out.df1<-aggregate(as.numeric(as.character(mini.df[,2])), by=list(Category=mini.df[,1]), FUN=sum)
+  colnames(out.df1)<-c(title1, title2)
+  out.df2<-aggregate(as.numeric(as.character(mini.df[,3])), by=list(Category=mini.df[,1]), FUN=sum)
+  colnames(out.df2)<-c(title1, title3)
+  out.df<-merge(out.df1,out.df2, by=title1)
+  out.df3<-unique(mini.df[,c(1,4)])
+  print(nrow(out.df3))
+  colnames(out.df3)<-c(title1, titleunit)
+  out.df<-merge(out.df,out.df3, by=title1)
+  out.df
+}
--- scripts/food.R
+++ scripts/food.R
@@ -56,62 +56,15 @@ rm(Ig_gsmf_forma_adqui)
 gsmf_forma_adqui[gsmf_forma_adqui== ""] <- NA
 #write.csv(gsmf_forma_adqui, "../outputs/gsmf_forma_adqui_Cali.csv")
 
-
-#############################################################################################
-#Aggregating consumption, quantities and units per article
-#############################################################################################
-
-gsdu_gas_dia.monthly.articles<-aggregate.artquavalunit(gsdu_gas_dia$GDU_ARTCLO, gsdu_gas_dia$GDU_CNTDAD_ADQURDA_MES_AJST , gsdu_gas_dia$GDU_VALOR_PGDO_ESTMDO_MES_AJST, gsdu_gas_dia$GDU_UDM_ESTANDAR,  "Product code", "Adjusted monthly quantity", "Adjusted monthly value", "Units")
-#gsdu_gas_dia.monthly.articles.values<-summarise(group_by(gsdu_gas_dia, GDU_ARTCLO), sum(as.numeric(as.character(GDU_VALOR_PGDO_ESTMDO_MES_AJST)), na.rm = TRUE))
-#View(gsdu_gas_dia.monthly.articles.values)
-gsdp_gas_dia.monthly.articles<-aggregate.artquaval(gsdp_gas_dia$GDP_ARTCLO, gsdp_gas_dia$GDP_CNTDAD_ADQURDA_MES_AJST, gsdp_gas_dia$GDP_VALOR_PGDO_ESTMDO_MES_AJST, "Product code", "Adjusted monthly quantity", "Adjusted monthly value")
-#gsdp_gas_dia.monthly.articles.values<-summarise(group_by(gsdp_gas_dia, GDP_ARTCLO), sum(as.numeric(as.character(GDP_VALOR_PGDO_ESTMDO_MES_AJST)), na.rm = TRUE))
-#View(gsdp_gas_dia.monthly.articles.values)
-gsdu_gasto_alimentos_cap_c.monthly.articles<-aggregate.artquaval(gsdu_gasto_alimentos_cap_c$ARTICULO, gsdu_gasto_alimentos_cap_c$CANTIDAD,gsdu_gasto_alimentos_cap_c$VALOR_MENSUAL_ALIMENTO,"Product code", "Adjusted monthly quantity", "Adjusted monthly value")
-#gsdu_gasto_alimentos_cap_c.monthly.articles.values<-summarise(group_by(gsdu_gasto_alimentos_cap_c, ARTICULO), sum(as.numeric(as.character(VALOR_MENSUAL_ALIMENTO)), na.rm = TRUE))
-#View(gsdu_gasto_alimentos_cap_c.monthly.articles.values)
-gsmf_compra.monthly.articles<-aggregate.twov(gsmf_compra$GMF_CMPRA_ARTCLO,gsmf_compra$GMF_CMPRA_VLR_PAGO_MES, "Product code", "Monthly value")
-#gsmf_compra.monthly.articles.values<-summarise(group_by(gsmf_compra, GMF_CMPRA_ARTCLO), sum(as.numeric(as.character(GMF_CMPRA_VLR_PAGO_MES)), na.rm = TRUE))
-#View(gsmf_compra.monthly.articles.values)
-gsmf_forma_adqui.monthly.articles<-aggregate.twov(gsmf_forma_adqui$GMF_ADQU_ARTCLO,gsmf_forma_adqui$GMF_ADQU_VLR_PAGO_MES, "Product code", "Monthly value")
-#gsmf_forma_adqui.monthly.articles.values<-summarise(group_by(gsmf_forma_adqui, GMF_ADQU_ARTCLO), sum(as.numeric(as.character(GMF_ADQU_VLR_PAGO_MES)), na.rm = TRUE))
-#View(gsmf_forma_adqui.monthly.articles.values)
-
-monthly.articles.list<-list(gsdu_gas_dia.monthly.articles
-                            , gsdp_gas_dia.monthly.articles
-                            , gsdu_gasto_alimentos_cap_c.monthly.articles
-                            , gsmf_compra.monthly.articles
-                            , gsmf_forma_adqui.monthly.articles)
-monthly.articles<-Reduce(function(...) merge(..., all=TRUE , by="Product code"), monthly.articles.list)
-rm(monthly.articles.list, gsdu_gas_dia.monthly.articles
-   , gsdp_gas_dia.monthly.articles
-   , gsdu_gasto_alimentos_cap_c.monthly.articles
-   , gsmf_compra.monthly.articles
-   , gsmf_forma_adqui.monthly.articles)
-
-monthly.articles$class<-substr(monthly.articles$`Product code`, 1, 2)
-monthly.articles<-monthly.articles[monthly.articles$class=="01"  | monthly.articles$class=="02",]
-View(monthly.articles)
-monthly.articles.values<-monthly.articles[,c(1,3,6,8,9,10)]
-View(monthly.articles.values)
-colnames(monthly.articles.values)<-c("Product code", 
-                                    "Value for the household unit database", 
-                                    "Value for the other members receiving income data",
-                                    "Value for the extrapolated food expenses data",
-                                    "Value for the less frequent expenses in cash or credit",
-                                    "Value for the less frequent expenses in gift, barter, etc."
-                                                                    )
-monthly.articles.values$`Total monthly value`<-rowSums (monthly.articles.values[,2:6], na.rm = TRUE, dims = 1)
-
-#library(xlsx)
-#write.xlsx(monthly.articles.values,"../outputs/monthly.articles.values.xlsx")
-
 #############################################################################################
 #Big table houses vs clases
 #############################################################################################
+library(datasets)
+library(dplyr)
+library(xlsx)
+library(reshape)
 
 #Load classes names
-library(xlsx)
 subclass.dane<-read.xlsx(m.labels[1], sheetName="subclass.dane",encoding="UTF-8")
 
 #File with ENIG_CODE, articles, values and quantities
@@ -121,8 +74,8 @@ gsdu_gas_dia.bigtable<-data.frame(gsdu_gas_dia$CODIGO_ENIG,
                                   gsdu_gas_dia$GDU_CNTDAD_ADQURDA_MES_AJST , 
                                   gsdu_gas_dia$GDU_UDM_ESTANDAR)
 colnames(gsdu_gas_dia.bigtable)<-c("ENIG_CODE", "Product code", "Adjusted monthly value", "Adjusted monthly quantity", "Units")
-#gsdu_gas_dia.bigtable.values<-summarise(group_by(gsdu_gas_dia.bigtable, `Product code`), sum(as.numeric(as.character(`Adjusted monthly value`)), na.rm = TRUE))
-#View(gsdu_gas_dia.bigtable.values)
+gsdu_gas_dia.bigtable.values<-summarise(group_by(gsdu_gas_dia.bigtable, `Product code`), sum(as.numeric(as.character(`Adjusted monthly value`)), na.rm = TRUE))
+View(gsdu_gas_dia.bigtable.values)
 
 
 gsdp_gas_dia.bigtable<-data.frame(gsdp_gas_dia$CODIGO_ENIG, 
@@ -165,24 +118,23 @@ bigtable.list<-list(gsdu_gas_dia.bigtable[,1:3],
 
 
 bigtable.all<-Reduce(function(...) rbind(...), bigtable.list)
-View(bigtable.all)
+#View(bigtable.all)
 
 
 bigtable.all$HOUSEID<-substr(bigtable.all$ENIG_CODE, 1, 7)
+#length(unique(bigtable.all$HOUSEID))
 bigtable.all$`Adjusted monthly value`<-as.numeric(as.character(bigtable.all$`Adjusted monthly value`)) 
 
-#Total expenses by product (incluiding all modules)
+#Total expenses by product (incluiding all modules, without households' information)
 bigtable.all.values<-summarise(group_by(bigtable.all, `Product code`), sum(`Adjusted monthly value`, na.rm = TRUE))
 View(bigtable.all.values)
 
 
 #For building the dataset at the level of products (Until here any classification can be used for all the products)
-library(datasets)
-library(dplyr)
 bigtable.outone<-summarise(group_by(bigtable.all, HOUSEID, `Product code`), sum(`Adjusted monthly value`, na.rm = TRUE))
 colnames(bigtable.outone)<-c("HOUSEID", "PRODUCTCODE", "VALUE")
 #bigtable.out.houseandproduct<-cast(bigtable.outone, HOUSEID ~ PRODUCTCODE)
-
+#length(unique(bigtable.outone$HOUSEID))
 
 #From here, only food products are used and the DANE's Classcode is used
 bigtable.outone$CLASSCODE<-substr(bigtable.outone$PRODUCTCODE, 1, 6)
@@ -191,23 +143,27 @@ bigtable.outone<-bigtable.outone[bigtable.outone$CLASSTWO=="01"  | bigtable.outo
 bigtable.outone<-merge(bigtable.outone, subclass.dane, by.x="CLASSCODE", by.y="subclass.code", all.x = TRUE)
 #View(bigtable.outone)
 
-#Total expenses by subclass (incluiding all modules)
+#Total expenses by subclass (incluiding all modules, without households' information)
 #bigtable.outone.subclass<-summarise(group_by(bigtable.outone, CLASSCODE), sum(VALUE, na.rm = TRUE))
 #View(bigtable.outone.subclass)
 
-library(reshape)
+#####
+# House and class table
 bigtable.out.houseandclass<-cast(bigtable.outone, HOUSEID ~ CLASSCODE + subclass.name.eng , sum, value="VALUE")
-View(bigtable.out.houseandclass)
+#View(bigtable.out.houseandclass)
 #bigtable.out.houseandclass.colsums<-as.data.frame(colSums(bigtable.out.houseandclass))
 #View(bigtable.out.houseandclass.colsums)
 #bigtable.out.houseandclass.colsums
 
 myNumCols <- which(unlist(lapply(bigtable.out.houseandclass, is.numeric)))
 bigtable.out.houseandclass[(nrow(bigtable.out.houseandclass) + 1), myNumCols] <- colSums(bigtable.out.houseandclass[, myNumCols], na.rm=TRUE)
-bigtable.out.houseandclass[nrow(bigtable.out.houseandclass), 1]<-"Total value of the class"
+bigtable.out.houseandclass[nrow(bigtable.out.houseandclass), 1]<-"Total monthly value of the class"
 #One row is deleted, because it corresponds to the row that contains the sums values
 bigtable.out.houseandclass[(nrow(bigtable.out.houseandclass) + 1), myNumCols] <- sapply(bigtable.out.houseandclass[, myNumCols], function(x)(sum(x > 0, na.rm=TRUE))-1)
 bigtable.out.houseandclass[nrow(bigtable.out.houseandclass), 1]<-"Number of households that consume the product"
+#For estimating the average values of the households that consumed the product
+bigtable.out.houseandclass[(nrow(bigtable.out.houseandclass) + 1), myNumCols] <- sapply(bigtable.out.houseandclass[(nrow(bigtable.out.houseandclass)-1):nrow(bigtable.out.houseandclass), myNumCols], function(x)(x[1]/x[2]))
+bigtable.out.houseandclass[nrow(bigtable.out.houseandclass), 1]<-"Average monthly consumption for the households that consumed the product"
 
 View(bigtable.out.houseandclass)
 #write.xlsx2(bigtable.out.houseandclass,"../outputs/bigtable.out.houseandclass.xlsx")
